ad6f8e4036a60770dcfcfc319a2d21ef79c716c8
LC-jhony <jhonyapm94@gmail.com>
2024-03-22 07:52:51 -0500

    update table viewoutput

--- app/Livewire/Calendario.php
+++ app/Livewire/Calendario.php
@@ -3,27 +3,50 @@
 namespace App\Livewire;
 
 use App\Models\Category;
+use App\Models\OutputProduct;
 use App\Models\Product;
 use Livewire\Component;
-use App\Models\OutputProduct;
 
 class Calendario extends Component
 {
     public $categoriesWithProducts;
 
+    public $dates;
+
+    public $productsByDate;
+
     public function mount()
     {
         // Obtener todas las categorías con sus productos asociados
         $this->categoriesWithProducts = Category::with('products')->get();
+
     }
 
     public function render()
     {
+        $days = OutputProduct::selectRaw('DAY(date) as dia')->groupBy('dia')->pluck('dia');
+        // Obtener los daysproducts
+        $products = Product::all(); // Ajusta según la estructura real de tu modelo Producto
 
-
+        // Inicializar el kardex con días y daysproducts
+        $this->dates = collect();
+        foreach ($days as $dia) {
+            $fila = ['dia' => $dia];
+            foreach ($products as $product) {
+                $total = OutputProduct::where('product_id', $product->id)
+                    ->whereDay('date', $dia)
+                    ->sum('quantity');
+                $fila[$product->id] = $total;
+            }
+            $this->dates->push($fila);
+        }
 
         return view('livewire.calendario', [
+
             'categoriesWithProducts' => $this->categoriesWithProducts,
+            'days' => $days,
+            'products' => $products,
+
         ]);
     }
 }
--- resources/views/livewire/calendario.blade.php
+++ resources/views/livewire/calendario.blade.php
@@ -9,7 +9,13 @@
             <th class="p-2 text-start uppercase text-white font-medium text-xs border border-white">U.M</th>
             <th class="p-2 text-start uppercase text-white font-medium text-xs border border-white">ingreso y egreso</th>
             <th class="p-2 text-start uppercase text-white font-medium text-xs border border-white">O/C</th>
-
+            @foreach ($days as $dia)
+            <th
+                class="px-6 py-3 text-center
+                text-xs font-medium whitespace-nowrap text-white uppercase tracking-wider dark:bg-gray-900
+                dark:text-gray-400 border border-white">
+                {{ $dia }}</th>
+        @endforeach
         </tr>
         </thead>
         <tbody colspan="6" class="bg-white divide-y divide-gray-200 border border-slate-100">
@@ -27,15 +33,29 @@
                     <td rowspan="2">{{ $product->um }}</td>
                     <td class="px-2 border">Entrada</td>
                     <td rowspan="2">{{$product->oc}}</td>
+                    @foreach ( $days as $day )
+                        <td>{{ $product->quantity }}</td>
+                    @endforeach
                     <!-- Agrega más columnas según tus necesidades -->
                 </tr>
                 <tr>
                     <td class="px-2 bg-gray-200 border">Salida</td>
+                    @foreach ($days as $day)
+                        <td class="px-2 bg-gray-200 border">
+                            {{ $dates->where('dia', $day)->pluck($product->id)->first() ?:'-' }}
+    
+                        </td>
+                    @endforeach
                 </tr>
+                
             @endforeach
         @endforeach
         </tbody>
     </table>
 </div>
+
+
+
+
 
 
